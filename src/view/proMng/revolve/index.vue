6a070adce98a568fe0925c41c714790b168050a9
lyl520zqm <LylOfYX>
2021-06-14 17:58:44 +0700

    提交最新源码

---
+++ src/view/proMng/revolve/index.vue
@@ -0,0 +1,245 @@
+<!--
+ * @Author: Lyl
+ * @Date: 2021-01-30 10:05:32
+ * @LastEditors: Lyl
+ * @LastEditTime: 2021-06-14 16:47:04
+ * @Description: 
+-->
+<template>
+  <div id="clothFlyPrint">
+    <view-container
+      title="漂染工作單打印"
+      v-loading="wloading"
+      element-loading-text="拼命加载中..."
+    >
+      <el-row class="btnList">
+        <el-button
+          type="success"
+          :disabled="!detail.runJobId"
+          @click="handleRowDBLClick(detail)"
+          >{{ this.$t("public.update") }}</el-button
+        >
+        <el-button type="primary" @click="add">{{
+          this.$t("public.add")
+        }}</el-button>
+        <el-button type="danger" :disabled="!detail.runJobId" @click="del">{{
+          this.$t("public.del")
+        }}</el-button>
+        <el-button type="primary" @click="print" :loading="wloading"
+          >打印</el-button
+        >
+        <el-button type="primary" @click="query">{{
+          this.$t("public.query")
+        }}</el-button>
+
+        <!-- <el-button type="warning" @click="close">{{
+          this.$t("public.close")
+        }}</el-button> -->
+      </el-row>
+      <el-row class="formBox">
+        <avue-form ref="form" :option="formOp" v-model="form"></avue-form>
+      </el-row>
+      <el-row class="crudBox">
+        <avue-crud
+          ref="crud"
+          id="crud"
+          :option="crudOp"
+          :data="crud"
+          :page.sync="page"
+          v-loading="loading"
+          @on-load="query"
+          @row-dblclick="handleRowDBLClick"
+          @current-row-change="cellClick"
+        ></avue-crud>
+      </el-row>
+      <el-dialog
+        id="colorMng_Dlg"
+        :visible.sync="dialogVisible"
+        fullscreen
+        width="100%"
+        append-to-body
+        :close-on-click-modal="false"
+        :close-on-press-escape="false"
+      >
+        <tem-dlg
+          v-if="dialogVisible"
+          ref="tem"
+          :detail="detail"
+          :isAdd="isAdd"
+          @close="dialogVisible = false"
+          @refresh="query"
+        ></tem-dlg>
+      </el-dialog>
+      <el-dialog
+        id="colorMng_Dlg"
+        :visible.sync="pdfDlg"
+        fullscreen
+        width="100%"
+        append-to-body
+        :close-on-click-modal="false"
+        :close-on-press-escape="false"
+      >
+        <view-container title="打印預覽">
+          <!-- <div class="btnList">
+            <el-button type="warning" @click="pdfDlg = false">{{
+              this.$t("public.close")
+            }}</el-button>
+            <el-button type="primary" @click="print2">打印</el-button>
+          </div> -->
+          <!--startprint-->
+          <embed
+            id="pdf"
+            style="width: 100vw; height: calc(100vh - 80px)"
+            :src="pdfUrl"
+          />
+          <!--endprint-->
+        </view-container>
+      </el-dialog>
+    </view-container>
+  </div>
+</template>
+<script>
+import { mainForm, mainCrud } from "./data";
+import { get, add, update, del, print } from "./api";
+import tem from "./temDlg";
+import html2Canvas from "html2canvas";
+import JsPDF from "jspdf";
+export default {
+  name: "",
+  components: {
+    temDlg: tem,
+  },
+  data() {
+    return {
+      formOp: mainForm(this),
+      form: {},
+      crudOp: mainCrud(this),
+      crud: [],
+      page: {
+        pageSize: 20,
+        currentPage: 1,
+        total: 0,
+      },
+      loading: false,
+      dialogVisible: false,
+      detail: {},
+      isAdd: false,
+      input: "",
+      wloading: false,
+      czsocket: {},
+      pdfDlg: false,
+      pdfUrl: "",
+    };
+  },
+  watch: {},
+  methods: {
+    query() {
+      this.loading = true;
+      this.detail = {};
+      for (let key in this.form) {
+        if (this.form[key] == "") {
+          delete this.form[key];
+        }
+      }
+      get(
+        Object.assign(this.form, {
+          rows: this.page.pageSize,
+          start: this.page.currentPage,
+        })
+      ).then((res) => {
+        this.crud = res.data.records;
+        this.crud.sort((a, b) => {
+          return a.workDate > b.workDate ? -1 : 1;
+        });
+        this.crud.forEach((item, i) => {
+          // item.custName = item.custCode;
+          // item.amount = item.amount.toFixed(2);
+          item.index = i + 1;
+        });
+
+        if (this.crud.length > 0) {
+          this.$refs.crud.setCurrentRow(this.crud[0]);
+        }
+        this.page.total = res.data.total;
+        this.loading = false;
+      });
+    },
+    print() {
+      // this.pdfDlg = true;
+      // this.pdfUrl =
+      //   process.env.API_HOST +
+      //   "/api/proBleadyeJob/buildWorkOrder?id=" +
+      //   this.detail.runJobId;
+      let domClass = document.get
+      var element = document.querySelector(domClass); // 这个dom元素是要导出pdf的div容器
+      html2Canvas(element).then(function (canvas) {
+        var contentWidth = canvas.width;
+        var contentHeight = canvas.height;
+
+        //一页pdf显示html页面生成的canvas高度;
+        var pageHeight = (contentWidth / 592.28) * 841.89;
+        //未生成pdf的html页面高度
+        var leftHeight = contentHeight;
+        //页面偏移
+        let position = 0;
+        //a4纸的尺寸[595.28,841.89]，html页面生成的canvas在pdf中图片的宽高
+        var imgWidth = 555.28;
+        var imgHeight = (592.28 / contentWidth) * contentHeight;
+        var pageData = canvas.toDataURL("image/jpeg", 1.0);
+        var pdf = new JsPDF("", "pt", [contentWidth, contentHeight]); //不分页
+        pdf.addImage(pageData, "JPEG", 0, 0, contentWidth, contentHeight);
+        pdf.save(title + ".pdf");
+      });
+    },
+
+    add() {
+      this.isAdd = true;
+      this.dialogVisible = true;
+    },
+    del() {
+      this.$tip
+        .cofirm(
+          this.$t("iaoMng.delTle7") +
+            this.detail.vatNo +
+            this.$t("iaoMng.delTle2"),
+          this,
+          {}
+        )
+        .then(() => {
+          del(this.detail.runJobId)
+            .then((res) => {
+              if (res.data.code === 200) {
+                this.$tip.success(this.$t("public.sccg"));
+                this.query();
+              } else {
+                this.$tip.error(this.$t("public.scsb"));
+              }
+            })
+            .catch((err) => {
+              this.$tip.error(this.$t("public.scsb"));
+            });
+        })
+        .catch((err) => {
+          this.$tip.warning(this.$t("public.qxcz"));
+        });
+    },
+    handleRowDBLClick(val) {
+      this.isAdd = false;
+      this.detail = val;
+      this.dialogVisible = true;
+    },
+    cellClick(val) {
+      this.detail = val;
+    },
+  },
+  created() {},
+  mounted() {
+    this.query();
+  },
+  beforeDestroy() {},
+};
+</script>
+<style lang='stylus'>
+#name {
+}
+</style>
