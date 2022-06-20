eea3005698c09d99a487d75f6601ee6d1907e84a
Symbol_Yang <Symbol_Yang@163.com>
2022-06-20 17:33:14 +0800

    织单用纱汇总新增领用量和胚布数字段

--- src/view/proMng/ProWeaveJobSummary/index.vue
+++ src/view/proMng/ProWeaveJobSummary/index.vue
@@ -2,7 +2,7 @@
  * @Author: Symbol_Yang
  * @Date: 2022-06-14 10:25:49
  * @LastEditors: Symbol_Yang
- * @LastEditTime: 2022-06-15 15:35:04
+ * @LastEditTime: 2022-06-20 08:30:12
  * @Description:
 -->
 <template>
