030e19b1b01da43e03a8a3e1d7a2d8f5d4baad8b
SymbolYang <1459752873@qq.com>
2022-06-21 16:57:50 +0800

    织胚明细初始化样式调整

--- src/view/proMng/print/proOutWeaveJob/meaveEmbyroDtl.vue
+++ src/view/proMng/print/proOutWeaveJob/meaveEmbyroDtl.vue
@@ -4,7 +4,7 @@
  * @Author: Symbol_Yang
  * @Date: 2022-06-20 11:17:59
  * @LastEditors: Symbol_Yang
- * @LastEditTime: 2022-06-21 13:59:23
+ * @LastEditTime: 2022-06-21 16:57:04
 -->
 <template>
     <view-container title="织胚明细" >
@@ -103,7 +103,7 @@
                     label: crudColLen.toString(),
                     cell: true,
                     placeholder: " ",
-                    width: 120
+                    width: 150
                 })
             },
             // 删除尺寸
